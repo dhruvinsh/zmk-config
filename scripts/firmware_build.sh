178eb7528b0d34278140c9cb28587432edd99c5d
Dhruvin Shah <dhruvin3@gmail.com>
2024-02-06 07:54:09 -0500

    chore(scripts): firmware_build works with more external module

--- scripts/firmware_build.sh
+++ scripts/firmware_build.sh
@@ -11,16 +11,16 @@ echo -e "${RED}---> Cleaning old build files..${NC}"
 rm -rf "$WORKSPACE_DIR"/../zmk-config/builds/*.uf2
 
 echo -e "${RED}---> Building lynx left side..${NC}"
-west build -p -d build/lynx_left -b nice_nano_v2 -- -DSHIELD="lynx_left nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR"/../zmk-config -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+west build -p -d build/lynx_left -b nice_nano_v2 -- -DSHIELD="lynx_left nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
 
 echo -e "${RED}---> Building lynx right side..${NC}"
-west build -p -d build/lynx_right -b nice_nano_v2 -- -DSHIELD="lynx_right nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR"/../zmk-config -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+west build -p -d build/lynx_right -b nice_nano_v2 -- -DSHIELD="lynx_right nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
 
 echo -e "${RED}---> Building calypso left side..${NC}"
-west build -p -d build/calypso_left -b nice_nano_v2 -- -DSHIELD="calypso_left" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR"/../zmk-config -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+west build -p -d build/calypso_left -b nice_nano_v2 -- -DSHIELD="calypso_left" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
 
 echo -e "${RED}---> Building calypso right side..${NC}"
-west build -p -d build/calypso_right -b nice_nano_v2 -- -DSHIELD="calypso_right" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR"/../zmk-config -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+west build -p -d build/calypso_right -b nice_nano_v2 -- -DSHIELD="calypso_right" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
 
 echo -e "${RED}---> Building reset firmware..${NC}"
 west build -p -d build/reset -b nice_nano_v2 -- -DSHIELD="settings_reset"
