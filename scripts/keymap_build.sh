71d6f230be6110de8cc76157539916a6e34ec810
Dhruvin Shah <dhruvin3@gmail.com>
2024-09-11 19:27:56 -0400

    refactor(script): keymap drawer needs to be installed via pipx

--- scripts/keymap_build.sh
+++ scripts/keymap_build.sh
@@ -6,6 +6,6 @@ if [ ! -d scripts ]; then
 fi
 
 echo -n "Building keymaps.."
-pipenv run keymap -c keymap_drawer.config.yaml parse -c 10 -z config/boards/shields/lynx/lynx.keymap >assets/lynx.yaml
-pipenv run keymap -c keymap_drawer.config.yaml draw --qmk-keyboard ferris/sweep assets/lynx.yaml >assets/lynx.svg
+keymap -c keymap_drawer.config.yaml parse -c 10 -z config/boards/shields/lynx/lynx.keymap >assets/lynx.yaml
+keymap -c keymap_drawer.config.yaml draw --qmk-keyboard ferris/sweep assets/lynx.yaml >assets/lynx.svg
 echo " Done"
