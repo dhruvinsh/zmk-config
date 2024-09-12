2d9fd277439deb4870e655d2d058c03a85ccbb6f
Dhruvin Shah <dshah@evertz.com>
2024-09-12 13:45:16 -0400

    feat: better text for draw script

--- scripts/keymap_build.sh
+++ scripts/keymap_build.sh
@@ -8,9 +8,9 @@ fi
 echo -n "Building Lynx keymaps.."
 keymap -c keymap_drawer.config.yaml parse -c 10 -z config/lynx.keymap >assets/lynx.yaml
 keymap -c keymap_drawer.config.yaml draw -k ferris/sweep assets/lynx.yaml >assets/lynx.svg
+echo " Done"
 
 echo -n "Building Calypso keymaps.."
 keymap -c keymap_drawer.config.yaml parse -c 10 -z config/calypso.keymap >assets/calypso.yaml
 keymap -c keymap_drawer.config.yaml draw -k corne_rotated -l LAYOUT_split_3x5_3 assets/calypso.yaml >assets/calypso.svg
-
 echo " Done"
