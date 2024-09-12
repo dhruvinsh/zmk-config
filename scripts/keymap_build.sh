fe815a9e52c987328e50fef78d341bccdf4f1de0
Dhruvin Shah <dshah@evertz.com>
2024-09-12 12:07:55 -0400

    chore: better messages for keymap script

--- scripts/keymap_build.sh
+++ scripts/keymap_build.sh
@@ -5,10 +5,12 @@ if [ ! -d scripts ]; then
 	exit 1
 fi
 
-echo -n "Building keymaps.."
+echo -n "Building Lynx keymaps.."
 keymap -c keymap_drawer.config.yaml parse -c 10 -z config/lynx.keymap >assets/lynx.yaml
 keymap -c keymap_drawer.config.yaml draw -k ferris/sweep assets/lynx.yaml >assets/lynx.svg
 
+echo -n "Building Calypso keymaps.."
 keymap -c keymap_drawer.config.yaml parse -c 10 -z config/calypso.keymap >assets/calypso.yaml
 keymap -c keymap_drawer.config.yaml draw -k corne_rotated -l LAYOUT_split_3x5_3 assets/calypso.yaml >assets/calypso.svg
+
 echo " Done"
