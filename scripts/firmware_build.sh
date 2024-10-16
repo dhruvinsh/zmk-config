cf8f7b6c18211ad33ee6fb77f60c20f5e452b63b
Dhruvin Shah <dshah@evertz.com>
2024-10-16 17:33:39 -0400

    feat(script): updating build scripts with eros

--- scripts/firmware_build.sh
+++ scripts/firmware_build.sh
@@ -25,8 +25,16 @@ west build -p -d build/calypso_right -b nice_nano_v2 -- -DSHIELD="calypso_right"
 echo -e "${RED}---> Building reset firmware..${NC}"
 west build -p -d build/reset -b nice_nano_v2 -- -DSHIELD="settings_reset"
 
+echo -e "${RED}---> Building eros left side..${NC}"
+west build -p -d build/eros_left -b nice_nano_v2 -- -DSHIELD="eros_left nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+
+echo -e "${RED}---> Building eros right side..${NC}"
+west build -p -d build/eros_right -b nice_nano_v2 -- -DSHIELD="eros_right nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+
 echo -e "${RED}---> Copying and renaming the build..${NC}"
 sleep 2
+cp build/eros_left/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/eros_left.uf2
+cp build/eros_right/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/eros_right.uf2
 cp build/lynx_left/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/lynx_left.uf2
 cp build/lynx_right/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/lynx_right.uf2
 cp build/calypso_left/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/calypso_left.uf2
