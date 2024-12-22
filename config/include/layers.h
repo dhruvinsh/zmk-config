87b2b7fe498083a7d23629bbe85e842e8a212788
Rahul <prabhune_rahul@yahoo.com>
2024-12-22 17:36:58 -0500

    changing default layer

--- config/include/layers.h
+++ config/include/layers.h
@@ -1,8 +1,9 @@
 #pragma once
 
 // Layers: Default, Qwerty, Navigation, Number, Symbol, System
-#define DEF 0
-#define QWE 1
+
+#define QWE 0
+#define DEF 1
 #define NAV 2
 #define NUM 3
 #define SYM 4
