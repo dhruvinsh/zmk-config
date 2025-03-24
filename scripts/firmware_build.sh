c540b0e54636a8a7693061d2eda43a2c6df1535f
Dhruvin Shah <dhruvin3@gmail.com>
2025-03-23 22:13:08 -0400

    refactor(scripts): add selective build options to firmware_build.sh
    Refactor the firmware build script to support building specific keyboards via command-line arguments. The script now:
    - Adds command-line parsing with keyboard selection options
    - Displays helpful usage information with --help flag
    - Organizes build commands into separate functions
    - Creates a more modular and maintainable structure
    - Adds improved output messages

--- scripts/firmware_build.sh
+++ scripts/firmware_build.sh
@@ -3,41 +3,158 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# Display usage information
+function usage {
+    echo "Usage: $0 [keyboard_name...]"
+    echo
+    echo "Build ZMK firmware for custom keyboards"
+    echo
+    echo "Options:"
+    echo "  keyboard_name    Name of keyboard to build (lynx, eros, helios, celeste, reset)"
+    echo "                   If no keyboard is specified, all keyboards will be built"
+    echo "  -h, --help       Display this help message"
+    echo
+    echo "Available keyboards:"
+    echo "  lynx             Build lynx (left and right sides)"
+    echo "  lynx_left        Build only lynx left side"
+    echo "  lynx_right       Build only lynx right side"
+    echo "  eros             Build eros (left and right sides)"
+    echo "  eros_left        Build only eros left side"
+    echo "  eros_right       Build only eros right side"
+    echo "  helios           Build helios"
+    echo "  celeste          Build celeste"
+    echo "  reset            Build nice_nano_v2 reset firmware"
+    echo "  all              Build all keyboards (default)"
+    exit 1
+}
+
+# Check for help argument
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    usage
+fi
+
 # NOTE: this will only works when run under the devcontainer
 # go to workspace dir, if missing that means its not container
 cd "$WORKSPACE_DIR"/app || exit 1
 
-echo -e "${RED}---> Cleaning old build files..${NC}"
-rm -rf "$WORKSPACE_DIR"/../zmk-config/builds/*.{uf2,bin}
+# Clean old build files
+function clean_builds {
+    echo -e "${RED}---> Cleaning old build files..${NC}"
+    rm -rf "$WORKSPACE_DIR"/../zmk-config/builds/*.{uf2,bin}
+}
 
-echo -e "${RED}---> Building lynx left side..${NC}"
-west build -p -d build/lynx_left -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="lynx_left" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+# Function to build lynx left
+function build_lynx_left {
+    echo -e "${RED}---> Building lynx left side..${NC}"
+    west build -p -d build/lynx_left -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="lynx_left" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+    cp build/lynx_left/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/lynx_left.uf2
+}
 
-echo -e "${RED}---> Building lynx right side..${NC}"
-west build -p -d build/lynx_right -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="lynx_right" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+# Function to build lynx right
+function build_lynx_right {
+    echo -e "${RED}---> Building lynx right side..${NC}"
+    west build -p -d build/lynx_right -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="lynx_right" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+    cp build/lynx_right/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/lynx_right.uf2
+}
 
-echo -e "${RED}---> Building eros left side..${NC}"
-west build -p -d build/eros_left -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="eros_left nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+# Function to build eros left
+function build_eros_left {
+    echo -e "${RED}---> Building eros left side..${NC}"
+    west build -p -d build/eros_left -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="eros_left nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+    cp build/eros_left/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/eros_left.uf2
+}
 
-echo -e "${RED}---> Building eros right side..${NC}"
-west build -p -d build/eros_right -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="eros_right nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+# Function to build eros right
+function build_eros_right {
+    echo -e "${RED}---> Building eros right side..${NC}"
+    west build -p -d build/eros_right -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="eros_right nice_view_adapter nice_view" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+    cp build/eros_right/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/eros_right.uf2
+}
 
-echo -e "${RED}---> Building helios..${NC}"
-west build -p -d build/helios -b blackpill_f411ce -S studio-rpc-usb-uart -- -DSHIELD="helios" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+# Function to build helios
+function build_helios {
+    echo -e "${RED}---> Building helios..${NC}"
+    west build -p -d build/helios -b blackpill_f411ce -S studio-rpc-usb-uart -- -DSHIELD="helios" -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+    cp build/helios/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/helios.uf2
+}
 
-echo -e "${RED}---> Building celeste..${NC}"
-west build -p -d build/celeste -b celeste -S studio-rpc-usb-uart -- -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word;$WORKSPACE_DIR/../zmk-ckled2001" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+# Function to build celeste
+function build_celeste {
+    echo -e "${RED}---> Building celeste..${NC}"
+    west build -p -d build/celeste -b celeste -S studio-rpc-usb-uart -- -DZMK_EXTRA_MODULES="$WORKSPACE_DIR/../zmk-config;$WORKSPACE_DIR/../zmk-tri-state;$WORKSPACE_DIR/../zmk-num-word;$WORKSPACE_DIR/../zmk-ckled2001" -DZMK_CONFIG="$WORKSPACE_DIR"/../zmk-config/config
+    cp build/celeste/zephyr/zmk.bin "$WORKSPACE_DIR"/../zmk-config/builds/celeste.bin
+}
 
-echo -e "${RED}---> Building nice_nano_v2 reset firmware..${NC}"
-west build -p -d build/reset -b nice_nano_v2 -- -DSHIELD="settings_reset"
+# Function to build reset firmware
+function build_reset {
+    echo -e "${RED}---> Building nice_nano_v2 reset firmware..${NC}"
+    west build -p -d build/reset -b nice_nano_v2 -- -DSHIELD="settings_reset"
+    cp build/reset/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/nice_nano_v2_reset.uf2
+}
 
-echo -e "${RED}---> Copying and renaming the build..${NC}"
-sleep 2
-cp build/eros_left/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/eros_left.uf2
-cp build/eros_right/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/eros_right.uf2
-cp build/lynx_left/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/lynx_left.uf2
-cp build/lynx_right/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/lynx_right.uf2
-cp build/reset/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/nice_nano_v2_reset.uf2
+# Create builds directory if it doesn't exist
+mkdir -p "$WORKSPACE_DIR"/../zmk-config/builds
 
-cp build/helios/zephyr/zmk.uf2 "$WORKSPACE_DIR"/../zmk-config/builds/helios.uf2
-cp build/celeste/zephyr/zmk.bin "$WORKSPACE_DIR"/../zmk-config/builds/celeste.bin
+# If no arguments provided, build all keyboards
+if [ $# -eq 0 ]; then
+    clean_builds
+    build_lynx_left
+    build_lynx_right
+    build_eros_left
+    build_eros_right
+    build_helios
+    build_celeste
+    build_reset
+    exit 0
+fi
+
+# Process arguments
+for arg in "$@"; do
+    case "$arg" in
+        "lynx")
+            build_lynx_left
+            build_lynx_right
+            ;;
+        "lynx_left")
+            build_lynx_left
+            ;;
+        "lynx_right")
+            build_lynx_right
+            ;;
+        "eros")
+            build_eros_left
+            build_eros_right
+            ;;
+        "eros_left")
+            build_eros_left
+            ;;
+        "eros_right")
+            build_eros_right
+            ;;
+        "helios")
+            build_helios
+            ;;
+        "celeste")
+            build_celeste
+            ;;
+        "reset")
+            build_reset
+            ;;
+        "all")
+            clean_builds
+            build_lynx_left
+            build_lynx_right
+            build_eros_left
+            build_eros_right
+            build_helios
+            build_celeste
+            build_reset
+            ;;
+        *)
+            echo "Unknown keyboard: $arg"
+            usage
+            ;;
+    esac
+done
+
+echo -e "${RED}---> Build completed successfully${NC}"
